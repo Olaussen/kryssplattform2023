6220a3081bd07f9b181bd8e14a034ab687a6e96f
aleksejunas <photo@seventyseven.no>
2023-09-07 22:08:04 +0200

    commited to the cause

--- solar-system-app/App.tsx
+++ solar-system-app/App.tsx
@@ -3,9 +3,12 @@ import React from "react";
 import { Text, View } from "react-native";
 
 export default function App() {
+  const message = "message!";
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hello There!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
