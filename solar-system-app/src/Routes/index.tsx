7c81a69485d9df1c122d6409aa081521b4ae09c7
Hauk Aleksander Olaussen <hauk.olaussen@aboveit.no>
2023-09-29 09:00:20 +0200

    feat: f6 del 1

--- solar-system-app/src/Routes/index.tsx
+++ solar-system-app/src/Routes/index.tsx
@@ -1,18 +1,72 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
 import React from "react";
 import HomePage from "../pages/HomePage";
-
+import Assets from "../Assets";
 
 const Tab = createBottomTabNavigator();
 const HomeRoutes: React.FC = () => {
   return (
     <Tab.Navigator initialRouteName="Start">
-      <Tab.Screen name="Start" component={HomePage} />
-      <Tab.Screen name="Search" component={HomePage} />
-      <Tab.Screen name="Favorites" component={HomePage} />
-      <Tab.Screen name="DetailPage" component={HomePage} />
+      <Tab.Screen
+        name="Start"
+        component={HomePage}
+        options={{
+          ...screenOptions,
+          tabBarIcon: ({ focused }) => (
+            <Assets.icons.Home style={iconStyle(focused)} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={HomePage}
+        options={{
+          ...screenOptions,
+          tabBarIcon: ({ focused }) => (
+            <Assets.icons.Search style={iconStyle(focused)} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={HomePage}
+        options={{
+          ...screenOptions,
+          tabBarIcon: ({ focused }) => (
+            <Assets.icons.Save style={iconStyle(focused)} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="DetailPage"
+        component={HomePage}
+        options={{
+          ...screenOptions,
+          tabBarIcon: ({ focused }) => (
+            <Assets.icons.Gallery style={iconStyle(focused)} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+const iconStyle = (focused: boolean) => ({
+  opacity: focused ? 1 : 0.5,
+  marginBottom: focused ? 6 : 0,
+});
+
+const screenOptions = {
+  headerShown: false,
+  tabBarStyle: {
+    borderColor: "#000",
+    backgroundColor: "#000",
+  },
+  tabBarActiveTintColor: "#ff0",
+  tabBarInactiveTintColor: "#fff",
+} as BottomTabNavigationOptions;
 
 export default HomeRoutes;
--- solar-system-app/src/pages/HomePage/index.tsx
+++ solar-system-app/src/pages/HomePage/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import Assets from "../../Assets";
 import { Colors } from "../../Styles/StyleGuide";
 import Header from "../../components/Header";
@@ -10,8 +10,8 @@ const HomePage: React.FC = () => {
   return (
     <View style={styles.container}>
       <Assets.images.BackgroundApp style={styles.bgImage} />
-      <Header />
-      <View>
+      <View style={styles.content}>
+        <Header />
         <Text style={styles.textWhite}>Categories</Text>
         <View style={styles.categoryList}>
           <CategoryButton
@@ -35,14 +35,17 @@ const HomePage: React.FC = () => {
             color="yellow"
           />
         </View>
-      </View>
-      <View>
-        <PlanetCard label="Mercury" PlanetImage={Assets.images.Mercury} />
-        <PlanetCard label="Venus" PlanetImage={Assets.images.Venus} />
-        <PlanetCard label="Earth" PlanetImage={Assets.images.Earth} />
-        <PlanetCard label="Mars" PlanetImage={Assets.images.Mars} />
-        <PlanetCard label="Jupiter" PlanetImage={Assets.images.Jupiter} />
-        <PlanetCard label="Saturn" PlanetImage={Assets.images.Saturn} />
+        <View>
+          <Text style={styles.textWhite}>Planets</Text>
+          <ScrollView horizontal style={styles.scrollView}>
+            <PlanetCard label="Mercury" PlanetImage={Assets.images.Mercury} />
+            <PlanetCard label="Venus" PlanetImage={Assets.images.Venus} />
+            <PlanetCard label="Earth" PlanetImage={Assets.images.Earth} />
+            <PlanetCard label="Mars" PlanetImage={Assets.images.Mars} />
+            <PlanetCard label="Jupiter" PlanetImage={Assets.images.Jupiter} />
+            <PlanetCard label="Saturn" PlanetImage={Assets.images.Saturn} />
+          </ScrollView>
+        </View>
       </View>
     </View>
   );
@@ -52,8 +55,11 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.brandBackground,
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
+    // justifyContent: "flex-start",
+    // alignItems: "center",
+  },
+  content: {
+    padding: 16,
   },
   categoryList: {
     flexDirection: "row",
@@ -66,6 +72,9 @@ const styles = StyleSheet.create({
   bgImage: {
     position: "absolute",
     zIndex: 0,
+  },
+  scrollView: {
+    marginTop: 10,
   },
 });
 
